6bff787fb05959d071a0810c317801ca8483bd20
NickKasten <nickkasten03@gmail.com>
2023-05-03 10:03:21 -0700

    reorg and beginning fixing labs

---
+++ Labs/Wk 9- Interface attached files May 3, 2023 954 AM/SpeakerTest.java
@@ -0,0 +1,26 @@
+public class SpeakerTest{
+
+	public static void main(String[] args){
+		System.out.println("\n");
+		TrainNav nytrain = new TrainNav();
+		nytrain.speak();
+		nytrain.announce("Next");
+		nytrain.announce("Grand Central");
+		nytrain.announce("Times Square");
+		System.out.println("\n");
+
+		MoodWeatherMan man = new MoodWeatherMan();
+		man.speak();
+		System.out.println("\n");
+		man.announce("Excited");
+		man.announce("Moody");
+		man.announce("Calm");
+		System.out.println("\n");
+		
+		Parrot parrot = new Parrot();
+		parrot.speak();
+		parrot.announce("Name");
+		parrot.announce("Age");	
+		
+	}
+}
